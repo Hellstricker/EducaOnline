6e3f5f1ba5963eca61a85e7f74495c9ae6acb8b5
Ozias Costa <oziasmcn@gmail.com>
2025-08-09 10:54:14 -0300

    Adicionadas entidades nos projetos Core

---
+++ src/building_blocks/EducaOnline.Core/Data/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using EducaOnline.Core.DomainObjects;
+
+namespace EducaOnline.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+    }
+}
---
+++ src/building_blocks/EducaOnline.WebAPI.Core/Identidade/JwtConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EducaOnline.WebAPI.Core.Identidade
+{
+    public static class IdentityConfiguration
+    {
+        public static WebApplicationBuilder AddJwtConfiguration(this WebApplicationBuilder builder)
+        {           
+
+            var appSettingSection = builder.Configuration.GetSection(nameof(JwtSettings));
+            builder.Services.Configure<JwtSettings>(appSettingSection);
+
+            var appsSettings = appSettingSection.Get<JwtSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettingSection.Key);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appsSettings!.Audiencia,
+                    ValidIssuer = appsSettings.Emissor
+                };
+            });
+            return builder;
+        }
+
+        public static WebApplication UseAuthConfiguration(this WebApplication app)
+        {
+            app.UseAuthorization();
+            app.UseAuthentication();
+            return app;
+        }
+    }
+}
