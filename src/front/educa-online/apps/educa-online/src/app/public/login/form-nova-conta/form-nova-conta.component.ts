4fba9dd483b91e56c5c3ec1a7b53f721b20212cb
Victor Lino <victor.lino@treeinova.com.br>
2025-09-23 20:52:40 -0300

    libs

---
+++ src/front/educa-online/apps/educa-online/src/app/public/login/form-nova-conta/form-nova-conta.component.ts
@@ -0,0 +1,50 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AlertComponent, AlertOptions } from '@educa-online/components';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'app-form-nova-conta',
+  templateUrl: 'form-nova-conta.component.html',
+  styleUrls: ['form-nova-conta.component.scss'],
+  standalone: false
+})
+
+export class FormaNovaContaComponent implements OnInit {
+  private _snackBar = inject(MatSnackBar);
+  
+  // form: NovaContaFormGroup;
+
+  constructor(
+    private router: Router,
+    // private usuarioService: UsuarioService
+  ) {
+    // this.form = new NovaContaFormGroup();
+  }
+
+  ngOnInit() {
+    return;
+  }
+
+  salvarConta(){
+    // this.usuarioService.post(this.form.value)
+    // .pipe(take(1))
+    // .subscribe(_ => {
+    //   this._snackBar.openFromComponent(AlertComponent, {
+    //     duration: 5000,
+    //     data: {
+    //       title: 'Sucesso!',
+    //       subtitle: 'conta criada!',
+    //       status: 'sucesso'
+    //     } as AlertOptions
+    //   });
+
+    //   this.voltar();
+    // })
+  }
+
+  voltar(): void {
+    this.router.navigate(['/login']);
+  }
+}
