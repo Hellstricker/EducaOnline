51a466ebd63963ce2c2b21215abfff3b39e8707a
Victor Lino <victor.lino@treeinova.com.br>
2025-09-27 18:12:12 -0300

    cadastro curso

--- src/front/educa-online/apps/educa-online/src/app/authenticated/curso/lista-curso/lista-curso.component.ts
+++ src/front/educa-online/apps/educa-online/src/app/authenticated/curso/lista-curso/lista-curso.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { CreateEditCursoComponent } from "../create-edit-curso/create-edit-curso.component";
+import { filter, take } from "rxjs";
+import { AlertComponent, AlertOptions } from "@educa-online/components";
 
 @Component({
   selector: 'app-lista-curso',
@@ -12,7 +15,7 @@ export class ListaCursoComponent implements OnInit {
   readonly dialog = inject(MatDialog);
   private _snackBar = inject(MatSnackBar);
 
-  displayedColumns: string[] = ['nome', 'dataCadastro', 'ativo', 'acoes'];
+  displayedColumns: string[] = ['nome', 'titulo', 'cargaHoraria', 'ativo', 'acoes'];
 
   cursos: any[] = [];
 
@@ -22,6 +25,33 @@ export class ListaCursoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getCursos();
+  }
 
+  getCursos(){
+    // Buscar cursos
+  }
+
+  novoCurso(): void {
+    const ref = this.dialog.open(CreateEditCursoComponent, {
+      width: '40rem',
+    });
+
+    ref.afterClosed()
+      .pipe(
+        take(1),
+        filter(data => data))
+      .subscribe(_ => {
+        this._snackBar.openFromComponent(AlertComponent, {
+          duration: 5000,
+          data: {
+            title: 'Sucesso!',
+            subtitle: 'Curso criado',
+            status: 'sucesso'
+          } as AlertOptions
+        });
+
+        this.getCursos();
+      });
   }
 }
