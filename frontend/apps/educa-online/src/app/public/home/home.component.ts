73dec3a267f948e032810125c5f2ef8657390e67
jairo bionez <jairo@treeinova.com.br>
2025-10-09 13:18:44 -0300

    home

---
+++ frontend/apps/educa-online/src/app/public/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CursoResponseModel } from '@educa-online/data';
+import { AuthService, ConteudoService } from '@educa-online/services';
+import { take } from 'rxjs';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
+})
+
+export class HomeComponent implements OnInit {
+    cursos: CursoResponseModel[] = [];
+    usuario: any;
+
+    constructor(private conteudoService: ConteudoService, private authService: AuthService, private router: Router) { 
+    }
+
+    ngOnInit() { 
+       this.usuario = this.authService.decodeToken();
+
+        this.conteudoService.get()
+            .pipe(take(1))
+            .subscribe(data => this.cursos = data)
+    }
+
+    redirectToLogin(matricular: boolean = false, cursoId: string = ''): void {
+        if(!this.usuario && matricular)
+            this.router.navigate([`/login`], {queryParams: {cursoId}});
+        else
+            this.router.navigate(['/login']);
+    } 
+}
